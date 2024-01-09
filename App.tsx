570fb69d2e4aa45a066e9c8610a0947cc4d0b16d
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-09 02:09:23 -0300

    add themes, config themeprovider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
+import theme from "@theme/index";
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
