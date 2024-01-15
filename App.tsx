fbac688d28ecff511a4d4c3475a86e0a0058c434
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-15 16:00:44 -0300

    init header componnet build and solve some errors with fonts rendering

--- App.tsx
+++ App.tsx
@@ -2,11 +2,11 @@ import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
 import theme from "@theme/index";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import { ActivityIndicator, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import { Loading } from "@components/Loading";
 
 export default function App() {
-  const [fontsLoaded] = useFonts(["Roboto_400Regular", "Roboto_700Bold"]);
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
   return (
     <ThemeProvider theme={theme}>
--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,5 +4,5 @@ import theme from '@theme/index';
 declare module 'styled-components' {
   type ThemeType = typeof theme;
 
-  export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType { }
 }
