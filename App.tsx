65ade5581fecb4927cecc1f7a5107b864dfeae5a
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-15 15:15:26 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
 import theme from "@theme/index";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts(["Roboto_400Regular", "Roboto_700Bold"]);
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
