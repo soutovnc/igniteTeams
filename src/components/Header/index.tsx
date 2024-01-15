dd82f55943474a40976908a81f3f58601d5dd938
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-15 18:24:47 -0300

    add icons lib

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
 import logoImg from '@assets/logo.png';
-import { Container, Logo } from './styles';
+import { BackButton, BackIcon, Container, Logo } from './styles';
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props ) {
   return (
     <Container>
+      {
+        showBackButton &&
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      }
+
       <Logo source={logoImg} />
     </Container>
   )
