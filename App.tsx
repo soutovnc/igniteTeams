1efddad1b32ce8ef92ad188b726198f07debfca8
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-15 15:27:39 -0300

    add some styles

--- App.tsx
+++ App.tsx
@@ -2,14 +2,20 @@ import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
 import theme from "@theme/index";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, StatusBar } from "react-native";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts(["Roboto_400Regular", "Roboto_700Bold"]);
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
