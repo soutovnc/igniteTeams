a44cba5fbddc0869fdc30091ca0fcbce299a44a9
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-16 23:38:02 -0300

    add listEmpty component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { ThemeProvider } from "styled-components/native";
-import { Groups } from "./src/screens/Groups";
-import theme from "@theme/index";
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import { StatusBar } from "react-native";
-import { Loading } from "@components/Loading";
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Groups /> : <Loading />}
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da SUCONPAR'])
+  const [groups, setGroups] = useState<string[]>([])
   
   return (
     <Container>
@@ -22,6 +23,10 @@ export function Groups() {
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
 
