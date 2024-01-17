b240fa5af3ccce8d71531f40fa8537aa3e074692
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-16 21:58:23 -0300

    types conflict resolve

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,20 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight
+        title="Turmas"
+        subtitle="jogue com sua turma"
+      />
+      <GroupCard
+        title="Galera do Ignite"
+      />
+
     </Container>
   );
 }
