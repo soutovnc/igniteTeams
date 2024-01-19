4970b4940e71e1dbdfc66fb13159cf7e159570e5
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-19 18:08:25 -0300

    create button icon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
 
 export function Players() {
   return (
@@ -11,6 +12,7 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+      <ButtonIcon />
     </Container>
   )
 }
