8f566ccff3035a81853c5863950d948ec4167538
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-19 18:00:06 -0300

    build players screen

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import theme from './src/theme';
 import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
 import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <Groups /> : <Loading/> }
+        { fontsLoaded ? <Players /> : <Loading/> }
     </ThemeProvider>
   );
 }
