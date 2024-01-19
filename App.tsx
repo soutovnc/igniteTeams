8243b4fe775d6332cc9d0ff4b89048a37c84db7f
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-18 23:51:25 -0300

    add newGroup component

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <Groups/> : <Loading/> }
+        { fontsLoaded ? <NewGroup/> : <Loading/> }
     </ThemeProvider>
   );
 }
