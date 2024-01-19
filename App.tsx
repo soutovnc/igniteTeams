b479aebb49fbf1e51d155e41b1d4bf5d156a6209
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-19 17:55:15 -0300

    refactoring, add css helper

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <NewGroup/> : <Loading/> }
+        { fontsLoaded ? <Groups /> : <Loading/> }
     </ThemeProvider>
   );
 }
--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,6 +1,6 @@
 import { UsersThree } from "phosphor-react-native";
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TouchableOpacity)`
   width: 100%;
@@ -10,13 +10,15 @@ export const Container = styled(TouchableOpacity)`
   flex-direction: row;
   align-items: center;
   padding: 24px;
-  margin-bottom: 12px
+  margin-bottom: 12px;
 `;
 
 export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.GRAY_200};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+${({ theme }) => css`
+  font-size: ${theme.FONT_SIZE.MD}px;
+  color: ${theme.COLORS.GRAY_200};
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+`};
 `;
 
 export const Icon = styled(UsersThree).attrs(({ theme }) => ({
