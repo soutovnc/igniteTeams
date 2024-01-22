4bc96cdb743473eab82b841954507a463c48aea3
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-22 00:00:21 -0300

    disable glitch in navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { AppRoutes } from './app.routes'
+import { useTheme } from 'styled-components'
+import { View } from 'react-native';
 
 export function Routes() {
+  const { COLORS } = useTheme();
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+
+    </View>
   )
 }
