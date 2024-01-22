0978598e2ca27621d3da7e9bae76d872c4665b5e
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-21 23:29:33 -0300

    build navigation structure

--- App.tsx
+++ App.tsx
@@ -6,8 +6,7 @@ import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
-import { NewGroup } from '@screens/NewGroup';
-import { Players } from '@screens/Players';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <Players /> : <Loading/> }
+        { fontsLoaded ? <Routes /> : <Loading/> }
     </ThemeProvider>
   );
 }
