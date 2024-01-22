2dfc19f46506b461c2cf80bb7160191fdd1a3e50
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-01-21 23:47:21 -0300

    implement back function in backIcon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import logoImg from '@assets/logo.png';
 import { BackButton, BackIcon, Container, Logo } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = {
   showBackButton?: boolean;
 }
 
 export function Header({ showBackButton = false }: Props ) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate('groups')
+  }
+  
   return (
     <Container>
       {
         showBackButton &&
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       }
