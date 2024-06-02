0905446d6d5b69bcfb08213bec1452db61bc8c49
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-06-02 18:28:10 -0300

    add remove player function, by group

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -19,6 +19,7 @@ import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
 
 type RouteParams = {
   group: string;
@@ -72,6 +73,17 @@ export function Players() {
     }
   }
 
+async function handlePlayerRemove(playerName: string) {
+  try {
+    await playerRemoveByGroup(playerName, group);
+
+    fetchPlayersByTeam()
+
+  } catch (error) {
+    console.log(error);
+    Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa.');
+  }
+}
 
   useEffect(() => {
     fetchPlayersByTeam();
@@ -128,7 +140,7 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard 
             name={item.name}
-            onRemove={() => {}}
+            onRemove={() => handlePlayerRemove(item.name)}
           />
         )}
         ListEmptyComponent={() => (
