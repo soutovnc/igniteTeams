3dca6f9e64925c9d630e89f2c7e1239b411c2a3e
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-06-02 18:20:46 -0300

    improve usability

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { Alert, FlatList } from "react-native";
+import { useState, useEffect, useRef } from "react";
+import { Alert, FlatList, TextInput } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 import { AppError } from "@utils/AppError";
@@ -33,6 +33,8 @@ export function Players() {
 
   const { group } = route.params as RouteParams;
 
+  const newPlayerNameInputRef = useRef<TextInput>(null);
+
   async function handleAddPlayer() {
     if (newPlayerName.trim().length === 0) {
       return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.');
@@ -45,6 +47,10 @@ export function Players() {
 
     try {
       await playerAddByGroup(newPlayer, group);
+
+      newPlayerNameInputRef.current?.blur();
+      setNewPlayerName('');
+
       await fetchPlayersByTeam();
     } catch (error) {
       if (error instanceof AppError) {
@@ -59,7 +65,6 @@ export function Players() {
   async function fetchPlayersByTeam() {
     try {
       const playersByTeam = await playersGetByGroupAndTeam(group, team);
-      setPlayers(playersByTeam);
       setPlayers(playersByTeam);
     } catch(error) {
       console.log(error);
@@ -82,10 +87,14 @@ export function Players() {
       />
 
       <Form>
-        <Input 
+        <Input
+          inputRef={newPlayerNameInputRef} 
           placeholder="Nome da pessoa"
+          value={newPlayerName}
           autoCorrect={false}
           onChangeText={setNewPlayerName}
+          onSubmitEditing={handleAddPlayer}
+          returnKeyType="done"
         />
         
         <ButtonIcon 
